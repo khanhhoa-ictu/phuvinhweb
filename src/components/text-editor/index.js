4ea21081b1d907c94e990bd5d797bce24f05eebe
HoaLK <khanhhoa01ictu@gmail.com>
2025-01-17 00:13:45 +0700

    update text editor

--- src/components/text-editor/index.js
+++ src/components/text-editor/index.js
@@ -1,9 +1,38 @@
 "use client";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import styles from "./styles.module.scss";
 import { uploadFile } from "@/service/image";
-import React, { useRef } from "react";
+import { useState, useEffect, useRef, useMemo } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import {
+	ClassicEditor,
+	Alignment,
+	Autoformat,
+	AutoImage,
+	Autosave,
+	BlockQuote,
+	Bold,
+	Essentials,
+	Heading,
+	ImageBlock,
+	ImageInline,
+	ImageInsert,
+	ImageInsertViaUrl,
+	ImageResize,
+	ImageStyle,
+	ImageTextAlternative,
+	ImageToolbar,
+	ImageUpload,
+	Indent,
+	IndentBlock,
+	Italic,
+	List,
+	Paragraph,
+	PasteFromOffice,
+	SimpleUploadAdapter,
+	TextTransformation
+} from 'ckeditor5';
+
+import "ckeditor5/ckeditor5.css";
 
 function customUploadAdapter(loader) {
   return {
@@ -27,39 +56,133 @@ function uploadPlugin(editor) {
 }
 
 const TextEditor = ({ data, onChange }) => {
+  const editorContainerRef = useRef(null);
   const editorRef = useRef(null);
+  const [isLayoutReady, setIsLayoutReady] = useState(false);
+
+  useEffect(() => {
+    setIsLayoutReady(true);
+
+    return () => setIsLayoutReady(false);
+  }, []);
+
+  const { editorConfig } = useMemo(() => {
+		if (!isLayoutReady) {
+			return {};
+		}
+
+		return {
+			editorConfig: {
+        extraPlugins: [uploadPlugin],
+				toolbar: {
+					items: [
+						'heading',
+						'|',
+						'bold',
+						'italic',
+						'|',
+						'insertImage',
+						'blockQuote',
+						'|',
+						'alignment',
+						'|',
+						'bulletedList',
+						'numberedList',
+						'outdent',
+						'indent'
+					],
+					shouldNotGroupWhenFull: false
+				},
+				plugins: [
+					Alignment,
+					Autoformat,
+					AutoImage,
+					Autosave,
+					BlockQuote,
+					Bold,
+					Essentials,
+					Heading,
+					ImageBlock,
+					ImageInline,
+					ImageInsert,
+					ImageInsertViaUrl,
+					ImageResize,
+					ImageStyle,
+					ImageTextAlternative,
+					ImageToolbar,
+					ImageUpload,
+					Indent,
+					IndentBlock,
+					Italic,
+					List,
+					Paragraph,
+					PasteFromOffice,
+					SimpleUploadAdapter,
+					TextTransformation
+				],
+				heading: {
+					options: [
+						{
+							model: 'paragraph',
+							title: 'Paragraph',
+							class: 'ck-heading_paragraph'
+						},
+						{
+							model: 'heading1',
+							view: 'h1',
+							title: 'Heading 1',
+							class: 'ck-heading_heading1'
+						},
+						{
+							model: 'heading2',
+							view: 'h2',
+							title: 'Heading 2',
+							class: 'ck-heading_heading2'
+						},
+						{
+							model: 'heading3',
+							view: 'h3',
+							title: 'Heading 3',
+							class: 'ck-heading_heading3'
+						},
+						{
+							model: 'heading4',
+							view: 'h4',
+							title: 'Heading 4',
+							class: 'ck-heading_heading4'
+						},
+						{
+							model: 'heading5',
+							view: 'h5',
+							title: 'Heading 5',
+							class: 'ck-heading_heading5'
+						},
+						{
+							model: 'heading6',
+							view: 'h6',
+							title: 'Heading 6',
+							class: 'ck-heading_heading6'
+						}
+					]
+				},
+				image: {
+					toolbar: ['imageTextAlternative', '|', 'imageStyle:inline', 'imageStyle:wrapText', 'imageStyle:breakText', '|', 'resizeImage']
+				},
+				licenseKey: "GPL",
+			}
+		};
+	}, [isLayoutReady]);
+  console.log(data)
   return (
     <div className={styles.textEditor}>
-      <CKEditor
-        editor={ClassicEditor}
-        config={{
-          extraPlugins: [uploadPlugin],
-          toolbar: [
-            "undo",
-            "redo",
-            "|",
-            "heading",
-            "alignment",
-            "|",
-            "bold",
-            "italic",
-            "|",
-            "uploadImage",
-            "blockQuote",
-            "|",
-            "bulletedList",
-            "numberedList",
-          ],
-          alignment: {
-            options: ["left", "center", "right", "justify"], // Các tùy chọn căn chỉnh
-          },
-        }}
-        data={data || ""}
-        onReady={(editor) => {
-          editorRef.current = editor; // Lưu trữ tham chiếu của CKEditor
-        }}
-        onChange={(event, editor) => onChange(event, editor)}
-      />
+      {editorConfig && (
+        <CKEditor
+          editor={ClassicEditor}
+          config={editorConfig}
+          data={data || ""}
+          onChange={(event, editor) => onChange(event, editor)}
+        />
+      )}
     </div>
   );
 };
